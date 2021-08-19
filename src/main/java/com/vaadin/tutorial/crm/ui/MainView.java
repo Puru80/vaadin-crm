6324e0d8533bcef5b03952fa3f2b5bcb64ab045d
Puru Agarwal <puru.agar99@gmail.com>
2021-08-19 15:37:18 +0530

    Second Message

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,9 +1,5 @@
-package com.vaadin.tutorial.crm;
+package com.vaadin.tutorial.crm.ui;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -11,22 +7,6 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        Button button = new Button("Click me");
-        DatePicker datePicker = new DatePicker("Pick A DATE");
-//        datePicker.setErrorMessage("Select A Date");
-
-        HorizontalLayout horizontalLayout = new HorizontalLayout(button, datePicker);
-        horizontalLayout.setDefaultVerticalComponentAlignment(Alignment.END);
-
-        add(horizontalLayout);
-
-        button.addClickListener(click -> {
-            if (datePicker.getValue()==null || datePicker.isEmpty()) {
-                datePicker.setErrorMessage("Select A DATE");
-            }
-            else
-                add(new Paragraph("Clicked: " + datePicker.getValue()));
-        });
     }
 
 }
