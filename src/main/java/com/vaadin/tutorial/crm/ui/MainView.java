74e6cfec3a373e969a7ef8886071b07cafee6f9c
Puru Agarwal <puru.agar99@gmail.com>
2021-08-23 09:48:05 +0530

    Grid completed with filter.

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -2,6 +2,8 @@ package com.vaadin.tutorial.crm.ui;
 
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
@@ -11,6 +13,7 @@ import com.vaadin.tutorial.crm.backend.service.ContactService;
 public class MainView extends VerticalLayout {
 
     private final Grid<Contact> contactGrid = new Grid<>(Contact.class);
+    private final TextField filterText = new TextField();
     private final ContactService contactService;
 
     public MainView(ContactService contactService) {
@@ -19,10 +22,18 @@ public class MainView extends VerticalLayout {
         setSizeFull();
 
         configureGrid();
+        configureFilter();
 
 
-        add(contactGrid);
+        add(filterText, contactGrid);
         updateList();
+    }
+
+    private void configureFilter() {
+        filterText.setPlaceholder("Filter by name... ");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
     }
 
     private void configureGrid() {
@@ -41,7 +52,7 @@ public class MainView extends VerticalLayout {
     }
 
     private void updateList() {
-        contactGrid.setItems(contactService.findAll());
+        contactGrid.setItems(contactService.findAll(filterText.getValue()));
     }
 
 
